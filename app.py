8384f0ba411f6d9467ac11dd726fe07b951bd9a6
securefileshare-project <securefileshare123@gmail.com>
2026-01-18 13:18:06 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -42,19 +42,21 @@ def send_otp_email(receiver, otp):
         msg["Subject"] = "Secure File Sharing - OTP"
         msg["From"] = SENDER_EMAIL
         msg["To"] = receiver
-
         msg.set_content(
             f"Your OTP is: {otp}\n\nUse it to download your file.\nDo NOT share this with anyone."
         )
 
-    with smtplib.SMTP("smtp.gmail.com", 587, timeout=20) as smtp:
-        smtp.starttls()
-        smtp.login(SENDER_EMAIL, APP_PASSWORD)
-        smtp.send_message(msg)
+        with smtplib.SMTP("smtp.gmail.com", 587, timeout=20) as smtp:
+            smtp.starttls()
+            smtp.login(SENDER_EMAIL, APP_PASSWORD)
+            smtp.send_message(msg)
+
         return True
+
     except Exception as e:
-        print("❌ OTP Mail sending failed:", e)
+        print("OTP Mail sending failed:", e)
         return False
+
 
 # -------- GLOBAL OTP SETTINGS --------
 generated_otp = None
@@ -229,6 +231,7 @@ def download_file():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
 
