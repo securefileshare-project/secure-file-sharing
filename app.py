9e92a289e43df4acfe541eee98b04e6c0c86970d
securefileshare-project <securefileshare123@gmail.com>
2026-02-01 12:26:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,19 +55,18 @@ def send_otp_email(receiver, otp):
     try:
         msg = EmailMessage()
         msg["Subject"] = "Secure File Sharing - OTP"
-        msg["From"] = f"{os.environ.get('SENDER_NAME')} <{os.environ.get('SENDER_EMAIL')}>"
+        msg["From"] = os.environ.get("SENDER_EMAIL")
         msg["To"] = receiver
         msg.set_content(
-            f"Your OTP is: {otp}\n\nUse it to download your file.\nDo NOT share this with anyone."
+            f"Your OTP is: {otp}\n\nDo NOT share this OTP with anyone."
         )
 
-        smtp_server = os.environ.get("SMTP_SERVER")
-        smtp_port = int(os.environ.get("SMTP_PORT"))
-        smtp_login = os.environ.get("SMTP_LOGIN")
-        smtp_password = os.environ.get("SMTP_PASSWORD")
+        smtp_server = os.environ.get("SMTP_SENDER")      # smtp-relay.brevo.com
+        smtp_port = int(os.environ.get("SMTP_PORT"))    # 587
+        smtp_login = os.environ.get("SMTP_LOGIN")        # a0f40500@smtp-brevo.com
+        smtp_password = os.environ.get("SMTP_PASSWORD") # Brevo SMTP KEY
 
-        with smtplib.SMTP(smtp_server, smtp_port, timeout=20) as smtp:
-            smtp.ehlo()
+        with smtplib.SMTP(smtp_server, smtp_port, timeout=30) as smtp:
             smtp.starttls()
             smtp.login(smtp_login, smtp_password)
             smtp.send_message(msg)
@@ -75,9 +74,8 @@ def send_otp_email(receiver, otp):
         return True
 
     except Exception as e:
-        print("MAIL ERROR:", e)
+        print("OTP Mail sending failed:", e)
         return False
-
 
 # ================= UPLOAD (SENDER) =================
 @app.route("/", methods=["GET", "POST"])
@@ -226,4 +224,5 @@ def admin_logs():
 # ================= RUN =================
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
 
